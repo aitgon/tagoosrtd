948fff422d0531e7d2ceb455c312b23c66472916
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2018-03-22 06:45:15 +0100

    REL

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Aitor GONZALEZ'
 author = 'Aitor GONZALEZ'
 
 # The short X.Y version
-version = ''
+version = '0.9.0'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = version
 
 
 # -- General configuration ---------------------------------------------------
